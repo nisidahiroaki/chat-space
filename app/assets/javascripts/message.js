af2aa1da74d7b37c7214f74fb44cafd78a75bd2b
nisidahiroaki <57944481+nisidahiroaki@users.noreply.github.com>
2019-12-03 14:01:08 +0900

    非同期通信　完了

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,70 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =`<div class="content__message">
+                  <div class="content__message__upper-info">
+                    <div class="content__message__upper-info__talker">
+                      ${message.name}
+                    </div>
+                    <div class="content__message__pper-info__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="content__message__text">
+                    <p class="lower-message__content">
+                      ${message.content}
+                    </p>
+                    <img class="lower-message__image">
+                      ${message.image}
+                  </div>
+                </div>`
+      return html;
+    } else {
+      var html =`<div class="content__message">
+                    <div class="content__message__upper-info">
+                      <div class="content__message__upper-info__talker">
+                        ${message.name}
+                      </div>
+                      <div class="content__message__pper-info__date">
+                        ${message.date}
+                      </div>
+                    </div>
+                    <div class="content__message__text">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    };
+  }
+
+
+
+
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      var html = buildHTML(message);
+      $('.content__messages').append(html);
+      $('.content__messages').animate({ scrollTop: $('.content__messages')[0].scrollHeight});
+      $('#new_message')[0].reset();
+      $('.form__submit').prop('disabled', false);
+    
+    })
+    .fail(function(){
+        alert("メッセージ送信に失敗しました")
+    });
+  })
+});
