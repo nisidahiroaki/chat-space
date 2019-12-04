b1e957395823a1c1d7834263646165a5495f76dd
nisidahiroaki <57944481+nisidahiroaki@users.noreply.github.com>
2019-12-04 21:20:32 +0900

    自動更新　完了

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,10 @@
-$(function(){
-
+$(function() {
   function buildHTML(message){
     var img = message.image ? `<img class="lower-message__image" src="${message.image}">`: "";
-    var html =`<div class="content__message">
+    var html =`<div class="content__message" data-message-id="${message.id}">
                 <div class="content__message__upper-info">
                   <div class="content__message__upper-info__talker">
-                    ${message.name}
+                    ${message.user_name}
                   </div>
                   <div class="content__message__pper-info__date">
                     ${message.date}
@@ -47,4 +46,32 @@ $(function(){
         alert("メッセージ送信に失敗しました")
     });
   })
+
+    var reloadMessages = function() {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){ 
+      var href = 'api/messages#index {:format=>"json"}'             
+      var last_message_id = $('.content__message:last').data('message-id');
+      last_message_id = $('.content__message:last').data("message-id");
+
+      $.ajax({       
+        url: href,     
+        type: 'get',
+        dataType: 'json',      
+        data: {id: last_message_id}
+      })
+
+      .done(function(messages) {   
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML)
+        $('.content__messages').animate({ scrollTop: $('.content__messages')[0].scrollHeight});
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    };
+  }
+  setInterval(reloadMessages, 7000);
 });
